6d23ad5b16d1d89afb1886479a9ed64175351a7c
Alex079 <>
2020-01-26 01:53:02 +0100

    use tool-cache

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,38 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as ac from '@actions/core'
+import * as atc from '@actions/tool-cache'
+import * as ag from '@actions/glob'
+import {join} from 'path'
+
+function extract(url: string): (file: string) => Promise<string> {
+  if (url.endsWith('.tar.gz') || url.endsWith('.tgz')) {
+    return atc.extractTar
+  } else if (url.endsWith('.zip')) {
+    return atc.extractZip
+  } else {
+    return atc.extract7z
+  }
+}
+
+function findFirst(expression: string): (folder: string) => Promise<string> {
+  return async (folder: string) =>
+    ag
+      .create(join(folder, expression))
+      .then(async globber => globber.glob())
+      .then(found => found[0])
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const url = ac.getInput('toolUrl', {required: true})
+    const expression = ac.getInput('archiveGlob')
+    await atc
+      .downloadTool(url)
+      .then(extract(url))
+      .then(findFirst(expression))
+      .then(ac.addPath)
+      .catch(ac.setFailed)
   } catch (error) {
-    core.setFailed(error.message)
+    ac.setFailed(error.message)
   }
 }
 
