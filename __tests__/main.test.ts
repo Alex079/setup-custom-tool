1ba85a05c822c046a6c4ab2d738178fda7734f58
Alex079 <>
2020-05-02 20:30:50 +0200

    update dependencies

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,11 +1,12 @@
+/* eslint-disable no-console */
 import * as process from 'process';
 import * as path from 'path';
-// import { rmdirSync } from 'fs';
+import { rmdirSync } from 'fs';
 import { ExecSyncOptions, execSync } from 'child_process';
 
 test('test all parameters', () => {
   const cmd = path.join(__dirname, '..', 'dist', 'index.js');
-  // const target = path.join(__dirname, '..', 'target');
+  const target = path.join(__dirname, '..', 'target');
   const options: ExecSyncOptions = {
     env: {
       ...process.env,
@@ -13,29 +14,29 @@ test('test all parameters', () => {
       INPUT_ARCHIVEGLOB: '*',
       INPUT_TOOLNAME: 'example',
       INPUT_TOOLVERSION: '2017.2.2',
-      INPUT_TOOLARCH: 'none'
-      // RUNNER_TEMP: target,
-      // RUNNER_TOOL_CACHE: target
+      INPUT_TOOLARCH: 'none',
+      RUNNER_TEMP: target,
+      RUNNER_TOOL_CACHE: target
     }
   };
   console.log('First run');
   console.log(execSync(`node ${cmd}`, options).toString());
   console.log('Second run - cache');
   console.log(execSync(`node ${cmd}`, options).toString());
-  // rmdirSync(target, { recursive: true });
+  rmdirSync(target, { recursive: true });
 });
 
 test('test required parameters', () => {
   const cmd = path.join(__dirname, '..', 'dist', 'index.js');
-  // const target = path.join(__dirname, '..', 'target');
+  const target = path.join(__dirname, '..', 'target');
   const options: ExecSyncOptions = {
     env: {
       ...process.env,
-      INPUT_ARCHIVEURL: 'https://file-examples.com/wp-content/uploads/2017/02/zip_2MB.zip'
-      // RUNNER_TEMP: target,
-      // RUNNER_TOOL_CACHE: target
+      INPUT_ARCHIVEURL: 'https://file-examples.com/wp-content/uploads/2017/02/zip_2MB.zip',
+      RUNNER_TEMP: target,
+      RUNNER_TOOL_CACHE: target
     }
   };
   console.log(execSync(`node ${cmd}`, options).toString());
-  // rmdirSync(target, { recursive: true });
+  rmdirSync(target, { recursive: true });
 });
